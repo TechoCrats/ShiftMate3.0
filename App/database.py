dc256b07f69809a1862aee81709ebc292e00c268
Sangeeta Santokee <santokee.sangeeta@gmail.com>
2025-12-02 20:30:58 -0400

    Updated database.py

--- App/database.py
+++ App/database.py
@@ -1,14 +1,18 @@
+  
+    # App/database.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
 
 db = SQLAlchemy()
 
 def get_migrate(app):
     return Migrate(app, db)
 
-def create_db():
-    db.create_all()
-    
 def init_db(app):
     db.init_app(app)
+
+    # Create tables if they don't exist
+    with app.app_context():
+        # Import models so SQLAlchemy knows them (User, etc.)
+        from App import models
+        db.create_all()
