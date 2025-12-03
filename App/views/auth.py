7cff365ee0640add9212c7ef8743b36f31d735cd
Sangeeta Santokee <santokee.sangeeta@gmail.com>
2025-12-02 20:06:47 -0400

    Update auth.py

--- App/views/auth.py
+++ App/views/auth.py
@@ -23,18 +23,89 @@ Page/Action Routes
 def identify_page():
     return render_template('message.html', title="Identify", message=f"You are logged in as {current_user.id} - {current_user.username}")
     
+from flask import Blueprint, render_template, jsonify, request, flash, redirect, url_for
+from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
+
+from .index import index_views
+from App.controllers import login
+
+auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
+
 
 @auth_views.route('/login', methods=['POST'])
 def login_action():
-    data = request.get_json()
-    token = login(data['username'], data['password'])
-    response = redirect(request.referrer)
-    if not token:
-        flash('Bad username or password given'), 401
+    # Accept both JSON and form-encoded
+    if request.is_json:
+        data = request.get_json(silent=True) or {}
     else:
-        flash('Login Successful')
-        set_access_cookies(response, token) 
+        data = request.form or {}
+
+    username = data.get('username')
+    password = data.get('password')
+
+    # Decide if the client wants JSON (Postman / JS) or HTML (browser form)
+    wants_json = request.is_json or (
+        request.accept_mimetypes['application/json']
+        >= request.accept_mimetypes['text/html']
+    )
+
+    # Basic validation
+    if not username or not password:
+        if wants_json:
+            return jsonify(message='Username and password required'), 400
+        flash('Username and password required')
+        next_url = request.referrer or url_for('index_views.index')
+        return redirect(next_url)
+
+    token = login(username, password)
+
+    if not token:
+        if wants_json:
+            return jsonify(message='Bad username or password given'), 401
+        flash('Bad username or password given')
+        next_url = request.referrer or url_for('index_views.index')
+        return redirect(next_url)
+
+    # Successful login
+    if wants_json:
+        # API-style response (for Postman / JS)
+        response = jsonify(message='Login successful', access_token=token)
+        set_access_cookies(response, token)
+        return response
+
+    # Browser-style response (redirect + flash)
+    flash('Login Successful')
+    next_url = request.referrer or url_for('index_views.index')
+    response = redirect(next_url)
+    set_access_cookies(response, token)
     return response
+
+# @auth_views.route('/login', methods=['POST'])
+# def login_action():
+#     # Accept JSON OR form-encoded
+#     if request.is_json:
+#         data = request.get_json(silent=True) or {}
+#     else:
+#         data = request.form  # from <form> submit
+
+#     username = data.get('username')
+#     password = data.get('password')
+
+#     token = login(username, password)
+
+#     # redirect target: never None
+#     next_url = request.referrer or url_for('index_views.index')
+
+#     if not token:
+#         flash('Bad username or password given')
+#         return redirect(next_url)
+
+#     flash('Login Successful')
+#     response = redirect(next_url)
+#     set_access_cookies(response, token)
+#     return response
+
+
 
 @auth_views.route('/logout', methods=['GET'])
 def logout_action():
